1c078c6a20cff696496aef12368ade87bbbd0be4
subbiah <subbiah.m@egrovesys.com>
2026-04-20 16:21:36 +0530

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,11 @@
+
+export default {
+  basePath: '/my-angular-app',
+  allowedHosts: [],
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
